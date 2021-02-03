5431ff00ed12abc654a148e4b869c0fb729cbc6e
Robert Flores <rrfloresgonzalez@gmail.com>
2021-02-03 13:16:51 -0300

    version se agregaron mas articulos a la tienda, se agregaron las descripciones de los productos. Tambien se agregaron nuevas animaciones en cursos y el index

---
+++ js/scroll-cursos.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#cursoFiltrado', {
+
+        duration: 1500,
+        origin: 'left',
+        distance: '200px'
+
+    });
+
+sr.reveal('#cursoEspresso', {
+
+        duration: 1500,
+        origin: 'right',
+        distance: '200px'
+
+    });
+
+sr.reveal('#cursoLatte', {
+
+        duration: 1500,
+        origin: 'left',
+        distance: '200px'
+
+    });
---
+++ js/scroll-index.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header__icono', {
+
+    	duration: 1500,
+    	origin: 'bottom',
+    	distance: '-150px'
+
+    });
+
+    sr.reveal('.header__titulo', {
+
+    	duration: 1500,
+    	origin: 'bottom',
+    	distance: '100px'
+
+    });
+
+     
+    
